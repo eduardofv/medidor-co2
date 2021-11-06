c6937fbbdb78fd99e211067066d9678bd0db1a7b
Eduardo Flores <eduardofv@gmail.com>
2021-11-05 20:03:48 -0600

    Nueva version del software de prueba para Node + 3 leds y diagramas

--- esphome/prueba1/run.sh
+++ esphome/prueba1/run.sh
@@ -1,1 +1,1 @@
-docker run --rm -v "${PWD}":/config -it esphome/esphome run medidor_co2_prueba1.yaml
+docker run --rm --device=/dev/tty.usbserial-0001 -v "${PWD}":/config -it esphome/esphome run medidor_co2_prueba1.yaml
